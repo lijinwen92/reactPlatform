5f3db2851e63eff97778faf4bbbcf4df71248418
lijiwen <932385314@qq.com>
2017-09-04 14:35:45 +0800

    配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,12 @@
 var webpack = require('webpack');
-moudle.exports = {
+module.exports = {
  
     //配置入口文件，入口文件可以以对象的形式配置，也可以以数组的形式配置,后缀名可以省略
-    /*
-     * 对象形式配置入口
-     * */
-    //entry:{
-    //    index:'./index'       //'./index.js'
-    //},
     /*
      * 数组形式配置入口
      * */
     entry: [
-        './index'       //'index.js'
+        './src/app'       //'index.js'
     ],
     //输出文件出口
     output: {
@@ -20,20 +14,21 @@ moudle.exports = {
          输出路径，在这我们要手动创建一个文件夹，名字可以自己命名，
          输出的文件路径是相对于本文件的路径
          * */
-        path: './dist',  //输出路径
-        filename: '[name].bundle.js'     //输出文件名，文件可以自己定义，[name]的意思是与入口文件的文件对应，可以不用[name]，
+        path: '/dist',  //输出路径
+        filename: '[name].js'     //输出文件名，文件可以自己定义，[name]的意思是与入口文件的文件对应，可以不用[name]，
     },
     /*
      * 标题：加载器（loaders）
      * 作用：需要下载不同别的加载器，如css，js，png等等
      * */
-    loaders: [
-        {test: /\.js?$/, loaders: ['babel-loader']} //babel加载器可以把jsx的语法转换为js的语法，也可以把es6的语法标准转换es5的语法标准
-        /*
-         * 你可以在这配置别的加载器，写法是一样的
-         * */
- 
-    ],
+    module: {
+        loaders: [{
+          test: /\.js$/, // Transform all .js files required somewhere with Babel
+          loader: 'babel-loader', //babel加载器可以把jsx的语法转换为js的语法，也可以把es6的语法标准转换es5的语法标准
+          exclude: /node_modules/,
+    
+        }]
+    },
     /*
      *
      * */
@@ -43,6 +38,12 @@ moudle.exports = {
          * */
         alias: {
             'd3': 'd3/d3.min.js'
-        }
+        },
+        
+        extensions: [
+            '.js',
+            '.jsx',
+            '.react.js',
+        ],
     }　　
 }
