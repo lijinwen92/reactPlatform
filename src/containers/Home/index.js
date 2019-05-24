f75888538073d4584d782b6e1c58b72a99bed206
lijinwen <932385314@qq.com>
2019-05-24 14:51:56 +0800

    修改

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Layout, Menu, Breadcrumb, Icon} from 'antd'
+import {Link} from 'react-router-dom'
+import menuObj from './menu'
 
 const {SubMenu} = Menu;
 const {Header, Content, Sider} = Layout;
@@ -7,6 +9,19 @@ const {Header, Content, Sider} = Layout;
 class Home extends React.Component {
 
     render() {
+        console.log(this,111)
+        const menuLoop = menuObj => menuObj.map(menu => {
+            if (menu.child) {
+                return <SubMenu
+                    key={menu.id}
+                    title={
+                        <span>{menu.title}</span>
+                    }
+                >{menuLoop(menu.child)}</SubMenu>
+            }
+            return <Menu.Item key={menu.id}>{menu.title}</Menu.Item>
+        })
+
         return (
             <Layout>
                 <Header className="header">
@@ -15,12 +30,13 @@ class Home extends React.Component {
                         theme="dark"
                         mode="horizontal"
                         defaultSelectedKeys={['2']}
-                        style={{lineHeight: '64px'}}
+                        style={{lineHeight: '64px', display: "inline-block"}}
                     >
                         <Menu.Item key="1">nav 1</Menu.Item>
                         <Menu.Item key="2">nav 2</Menu.Item>
                         <Menu.Item key="3">nav 3</Menu.Item>
                     </Menu>
+                    <Link to="/login">登录</Link>
                 </Header>
                 <Layout>
                     <Sider width={200} style={{background: '#fff'}}>
@@ -30,53 +46,15 @@ class Home extends React.Component {
                             defaultOpenKeys={['sub1']}
                             style={{height: '100%', borderRight: 0}}
                         >
-                            <SubMenu
-                                key="sub1"
-                                title={
-                                    <span><Icon type="user"/>subnav 1</span>
-                                }
-                            >
-                                <Menu.Item key="1">option1</Menu.Item>
-                                <Menu.Item key="2">option2</Menu.Item>
-                                <Menu.Item key="3">option3</Menu.Item>
-                                <Menu.Item key="4">option4</Menu.Item>
-                            </SubMenu>
-                            <SubMenu
-                                key="sub2"
-                                title={
-                                    <span><Icon type="laptop"/>subnav 2</span>
-                                }
-                            >
-                                <Menu.Item key="5">option5</Menu.Item>
-                                <Menu.Item key="6">option6</Menu.Item>
-                                <Menu.Item key="7">option7</Menu.Item>
-                                <Menu.Item key="8">option8</Menu.Item>
-                            </SubMenu>
-                            <SubMenu
-                                key="sub3"
-                                title={
-                                    <span><Icon type="notification"/>subnav 3</span>
-                                }
-                            >
-                                <Menu.Item key="9">option9</Menu.Item>
-                                <Menu.Item key="10">option10</Menu.Item>
-                                <Menu.Item key="11">option11</Menu.Item>
-                                <Menu.Item key="12">option12</Menu.Item>
-                            </SubMenu>
+                            {menuLoop(menuObj)}
                         </Menu>
                     </Sider>
-                    <Layout style={{padding: '0 24px 24px'}}>
-                        <Breadcrumb style={{margin: '16px 0'}}>
-                            <Breadcrumb.Item>Home</Breadcrumb.Item>
-                            <Breadcrumb.Item>List</Breadcrumb.Item>
-                            <Breadcrumb.Item>App</Breadcrumb.Item>
-                        </Breadcrumb>
+                    <Layout style={{padding: '24px 24px'}}>
                         <Content
                             style={{
                                 background: '#fff',
-                                padding: 24,
+                                padding: 20,
                                 margin: 0,
-                                minHeight: 280,
                             }}
                         >
                             Content
---
+++ src/containers/Home/menu.js
@@ -0,0 +1,18 @@
+const menuObj = [
+    {
+        id:"1",
+        title:"菜单1"
+    },
+    {
+        id:'2',
+        title:"菜单2",
+        child:[
+            {
+                id:"2-1",
+                title:"菜单2-1"
+            }
+        ]
+    }
+]
+
+export default menuObj
